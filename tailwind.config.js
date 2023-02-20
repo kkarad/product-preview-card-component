200e95775ad242edff15aaf0717327b49dea4a0b
Konstantinos Karadamoglou <kkarad@gmail.com>
2023-02-20 23:41:42 +0000

    added styles to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+                white: "hsl(0, 0%, 100%)",
+                "dark-cyan": "hsl(158, 36%, 37%)",
+                "cream": "hsl(30, 38%, 92%)",
+                "very-dark-blue": "hsl(212, 21%, 14%)",
+                "dark-grayish-blue": "hsl(228, 12%, 48%)"
+            },
+            fontFamily: {
+                "heading": ['"Fraunces"', 'serif'],
+                "display": ['"Montserrat"', 'sans-serif']
+            }
+        },
+    },
+    plugins: [],
 }
